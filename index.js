7a66d9d3a60b917144baac83b677d3634530ee47
Chad Altemose <chad@chadzilla.com>
2014-12-13 14:12:04 -0500

    added results folder for file i/o and ignored it.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var mod = require('./mod');
+var speedtest = require('./speedtest');
 
 var options = {
   testFile: 's3.amazonaws.com/zilla-nodetest/amaranth.png',
-  minutes: 5 // 0 forces one-time run for testing
+  minutes: 0 // 0 forces one-time run for testing
 }
 
-mod.run(options);
+speedtest.run(options);
